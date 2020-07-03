f680054beabaa75d9f7bda056b664a6451af4504
Mikita Liaukovich <nicklevkovic@gmail.com>
2020-07-04 01:50:41 +0300

    feat: add map generation

---
+++ Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour
+{
+    public int maxMapWidth = 10;
+    public int maxMapHeight = 10;
+    public List<List<GameObject>> map = new List<List<GameObject>>();
+    public Transform tilesWrapper;
+
+    public Transform prefab;
+    
+    void Start()
+    {
+        CreateMap();
+    }
+
+    void CreateMap()
+    {
+        for(float h = 0; h < maxMapHeight; h++)
+            for(float w = 0; w < maxMapWidth; w++)
+            {
+                Transform newTile = Instantiate(prefab, new Vector3(w + (h % 2 / 2), 0, h * 0.866f), Quaternion.identity);
+                newTile.parent = tilesWrapper;
+            }
+    }
+
+    void Update()
+    {
+        
+    }
+}
